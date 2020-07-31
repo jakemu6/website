1c46e8d44f6aaa389b83a56465d92f13d5695e23
Jake Mu <jakemu6@gmail.com>
2020-07-31 16:27:51 +1000

    first commit

--- src/components/aboutText.js
+++ src/components/aboutText.js
@@ -1,64 +1,74 @@
 import React from "react"
 import MEDIA from '../helpers/mediaTemplates';
+import BREAKPOINTS from '../constants/breakpoints';
+
 import styled from "styled-components"
+import { useMediaQuery } from 'react-responsive'
+
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: (BREAKPOINTS.TABLET) })
+  return isDesktop ? children : null
+}
+const Tablet = ({ children }) => {
+  const isTablet = useMediaQuery({ minWidth: (BREAKPOINTS.PHONE), maxWidth: (BREAKPOINTS.TABLET) })
+  return isTablet ? children : null
+}
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: (BREAKPOINTS.PHONE) })
+  return isMobile ? children : null
+}
 
 
 const Container = styled.div`
-
-background: white;
 flex-direction: column;
 display: flex;
 top: 2rem;
 padding-right: 2rem;
-
-
 position: fixed;
 align-items: left;
-left: 45%;
+left: 52%;
 margin: 0;
 
-
-
 ${MEDIA.TABLET`
-    background: white;
-    margin: 2rem;
-    margin-bottom: 0rem;
 
-    max-width: 95%;
+
+    max-width: 100%;
     position: static;
-    align-items: center;
-    padding: 2rem;
+    align-items: left;
+    padding: 1rem;
     padding-bottom: 0;
-
-
-
+    z-index: 10;
 
   `};
 ${MEDIA.PHONE`
-    background: white;
-    margin: 2rem;
-    margin-bottom: 0rem;
-
-    max-width: 95%;
-    position: static;
-    align-items: center;
-    padding: 2rem;
-    padding-bottom: 0;
 
 
-
-
-
-
+  max-width: 100%;
+  position: static;
+  align-items: left;
+  padding: 1rem;
+  padding-bottom: 0;
+  z-index: 10;
   `};
 `
 
 
 const AboutText = ({ children }) => {
+
+
   return(
     <Container>
+      <Desktop>
       {children}
+      </Desktop>
+      <Tablet>
+      {children}
+      </Tablet>
+      <Mobile>
+      {children}
+      </Mobile>
     </Container>
+
   )
 }
 
